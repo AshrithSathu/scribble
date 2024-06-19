2c6d774608f7f92aa8e3aad0ba7a55695ba5a6f2
AshrithSathu <sathuashrith@gmail.com>
2024-06-19 16:35:50 +0530

    next auth setup done

--- server/schema.ts
+++ server/schema.ts
@@ -1,1 +1,42 @@
-import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  timestamp,
+  pgTable,
+  text,
+  primaryKey,
+  integer,
+  uuid,
+} from "drizzle-orm/pg-core";
+import type { AdapterAccount } from "next-auth/adapters";
+
+export const users = pgTable("user", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: text("name"),
+  email: text("email").unique().notNull(),
+  password: text("password"),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
+});
+
+export const accounts = pgTable(
+  "account",
+  {
+    userId: uuid("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount["type"]>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  })
+);
