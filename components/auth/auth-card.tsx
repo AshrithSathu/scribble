8d36adedfa1d45b0e4772e9d86e0547ea84d7379
AshrithSathu <sathuashrith@gmail.com>
2024-06-21 04:37:45 +0530

    basic ui commits -- sleep time

---
+++ components/auth/auth-card.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import Socials from "./socials";
+import { BackButton } from "./back-button";
+type AuthCardProps = {
+  children: React.ReactNode;
+  cardTitle: string;
+  backButtonHref: string;
+  backButtonLabel: string;
+  showSocials?: boolean;
+};
+
+export const AuthCard = ({
+  children,
+  cardTitle,
+  backButtonHref,
+  backButtonLabel,
+  showSocials,
+}: AuthCardProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{cardTitle}</CardTitle>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocials && (
+        <CardFooter>
+          <Socials />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton />
+      </CardFooter>
+    </Card>
+  );
+};
---
+++ components/auth/socials.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "../ui/button";
+
+export default function Socials() {
+  return (
+    <div>
+      <Button> Sign in With Google</Button>
+      <Button> Sign in With Github</Button>
+    </div>
+  );
+}
